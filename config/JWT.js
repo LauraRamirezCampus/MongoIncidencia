3fa5b51de56c32591404672cbed8c4c152d859ba
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-26 19:50:36 -0500

    feat: :white_check_mark: modificacion de token
    se modifica el validador de token

--- config/JWT.js
+++ config/JWT.js
@@ -1,45 +1,54 @@
-import { SignJWT, jwtVerify } from "jose";
+import { SignJWT, jwtVerify } from "jose"
 import {con} from "../db/atlas.js";
 import { ObjectId } from "mongodb";
 import dotenv from 'dotenv';
 dotenv.config("../");
 
-const conexion = await con();
-const generaToken = async(req,res,next)=>{
-    if(Object.keys(req.body).length === 0) return res.status(400).send({message:"no se han enviado los datos"});
-    const result = await conexion.collection('usuario').findOne(req.body);
-    if(!result) return res.status(401).send({message:"no existe el rol"});
+const conexionDB = await con();
+const crearToken = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0) return res.status(400).send({mesaage: "Datos no enviados"});
+    const result = await conexionDB.collection('usuario').findOne(req.body);
+    console.log(result);
+    if (!result) return res.status(401).send({mesaage: "Usuario no encontrado"});
     const encoder = new TextEncoder();
     const id = result._id.toString();
-    console.log(result);
     const jwtConstructor = await new SignJWT({ id: id})
         .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
         .setIssuedAt()
         .setExpirationTime('3h')
-        .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
-        res.status(200).send({mesaage: jwtConstructor})
-        req.data = {status: 200, mesaage: jwtConstructor};
+        .sign(encoder.encode(process.env.JWT_SECRET));
+        res.status(200).send({JWT:jwtConstructor})
+    req.data = {status: 200, mesaage: jwtConstructor};
+    next(); 
 }
 
-const verifyToken = async(req,token)=>{
-    console.log(token);
+
+
+const verifyToken = async (req,res,next)=>{
+    const {authorization} = req.headers;
+    if (!authorization) return res.status(400).send({status: 400, token: "Token no enviado"});
     try {
         const encoder = new TextEncoder();
-        const jwtData =await jwtVerify(
-            token,
+        const jwtData = await jwtVerify(
+            authorization,
             encoder.encode(process.env.JWT_PRIVATE_KEY)
-        );
-        let resul = await conexion.collection('usuario').findOne({
-            _id:new ObjectId(jwtData.payload.id),
-            [`permisos.${req.baseUrl}`]:`${req.headers["accept-version"]}`
-        });
-        let {_id,permisos, ...usuario} = res;
-        return usuario;
-        
-    } catch (error) {
-        return false
-        
-    }
+            );
+            let res = await conexionDB.collection('usuario').findOne(
+                {
+                    _id:new ObjectId(jwtData.payload.id),
+                    [`permisos.${req.baseUrl}`]: `${req.headers["accept-version"]}`
+                }
+                );
+                
+            let {_id, permisos, ...usuario} = res;
+            return usuario;
+            } catch (error) {
+                next()
+            }
 }
 
-export  {generaToken,verifyToken}
+
+export {
+    crearToken,
+    verifyToken
+}
--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,11 @@
 import passport from "passport";
 import { Strategy as  BearerStrategy} from "passport-http-bearer";
-import { validarToken } from "./JWT.js";
+import { verifyToken } from "./JWT.js";
 
 passport.use(new BearerStrategy( 
   { passReqToCallback: true },
   async function(req, token, done) {
-    const usuario =  await validarToken(req, token)
+    const usuario =  await verifyToken(req, token)
     if (!usuario) return done(null, false);
     return done(null, usuario);
   }
